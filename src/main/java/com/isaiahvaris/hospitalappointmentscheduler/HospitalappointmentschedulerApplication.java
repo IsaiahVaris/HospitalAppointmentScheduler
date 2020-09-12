041f96f0deb5db8cd63d88a1f9f4fad7f86b0cba
Isaiah Ifeanyichukwu <60203048+IsaiahVaris@users.noreply.github.com>
2020-09-12 09:13:36 +0100

    initial commit

---
+++ src/main/java/com/isaiahvaris/hospitalappointmentscheduler/HospitalappointmentschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.isaiahvaris.hospitalappointmentscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalappointmentschedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalappointmentschedulerApplication.class, args);
+    }
+
+}
